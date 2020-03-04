c7ccbf1e0a3bf68184f5a05dfd2dc66af3eb08a0
kunal <purikunal22@gmail.com>
2020-03-05 01:51:21 +0530

    Deployed Configuration

--- src/main/java/com/brtracker/coronavirusapi/CoronavirusApiApplication.java
+++ src/main/java/com/brtracker/coronavirusapi/CoronavirusApiApplication.java
@@ -2,16 +2,24 @@ package com.brtracker.coronavirusapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableFeignClients
-public class CoronavirusApiApplication {
+public class CoronavirusApiApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CoronavirusApiApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CoronavirusApiApplication.class);
+    }
+
 
 }
